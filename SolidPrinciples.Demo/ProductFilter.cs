ecc899a435249070bf653746da5c76d79625ea50
Janier Davila <janier_davila@yahoo.com>
2013-10-20 09:06:47 -0400

    more files

--- SolidPrinciples.Demo/ProductFilter.cs
+++ SolidPrinciples.Demo/ProductFilter.cs
@@ -5,10 +5,6 @@ namespace SolidPrinciples.Demo.OCP
 
     public class ProductFilter
     {
-
-
-
-
         public IEnumerable<Product> ByColor(IList<Product> products, ProductColor productColor)
         {
             foreach (var product in products)
@@ -17,66 +13,26 @@ namespace SolidPrinciples.Demo.OCP
                     yield return product;
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //public IEnumerable<Product> BySize(IList<Product> products,
-        //                                    ProductSize productSize)
-        //{
-        //    foreach (var product in products)
-        //    {
-        //        if ((product.Size == productSize))
-        //            yield return product;
-        //    }
-        //}
-
-
-
-
-
-
-        //public IEnumerable<Product> ByColorAndSize(IList<Product> products,
-        //                                            ProductColor productColor,
-        //                                            ProductSize productSize)
-        //{
-        //    foreach (var product in products)
-        //    {
-        //        if ((product.Color == productColor) &&
-        //            (product.Size == productSize))
-        //            yield return product;
-        //    }
-        //}
+        public IEnumerable<Product> BySize(IList<Product> products,
+                                            ProductSize productSize)
+        {
+            foreach (var product in products)
+            {
+                if ((product.Size == productSize))
+                    yield return product;
+            }
+        }
+        public IEnumerable<Product> ByColorAndSize(IList<Product> products,
+                                                    ProductColor productColor,
+                                                    ProductSize productSize)
+        {
+            foreach (var product in products)
+            {
+                if ((product.Color == productColor) &&
+                    (product.Size == productSize))
+                    yield return product;
+            }
+        }
 
        
     }
